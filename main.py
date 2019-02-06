a6c7f62dea72652ddafa9f4d1f7a05ac219070ff
ivzhang1 <izhang1@stuy.edu>
2019-02-06 15:33:06 -0500

    move stuff

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from display import *
 from draw import *
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
 
 # OCTANT 1 TEST
+#color = [ 0, 255, 0 ]
 # draw_line( 0, 0, 1000, 250, screen, color)
 # draw_line( 0, 0, 1000, 500, screen, color)
 # draw_line( 0, 0, 1000, 750, screen, color)
 # draw_line( 0, 0, 1000, 1000, screen, color)
 
 # OCTANT 2 TEST
+color = [ 255, 0, 0 ]
 draw_line( 0, 0, 250, 1000, screen, color)
 draw_line( 0, 0, 500, 1000, screen, color)
 draw_line( 0, 0, 750, 1000, screen, color)
