a4dbca65b72ef438af08df684a8912aaa962f34c
ivzhang1 <izhang1@stuy.edu>
2019-02-06 22:15:08 -0500

    done

--- draw.py
+++ draw.py
@@ -6,32 +6,63 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     a = (y1-y0)
     b = -1 * (x1-x0)
 
-    if(abs(a) <= abs(b)):
-        #Octant I
-        d = 2*a+b
+    if(y0 <= y1):
+        if(abs(a) <= abs(b)): # IF dy <= dx, slope <= 1
+            #Octant I, V
+            d = 2*a+b
 
-        while x0 <= x1:
+            while x0 <= x1:
 
-            #PLOT
-            plot(screen, color, x0, y0)
-            if d > 0:
+                #PLOT
+                plot(screen, color, x0, y0)
+                if d > 0:
+                    y0+=1
+                    d += 2*b
+
+                x0+=1
+                d += 2*a
+        else:
+
+            #Octant II, VI
+            d = a+2*b
+
+            while y0 <= y1:
+
+                #PLOT
+                plot(screen, color, x0, y0)
+                if d < 0:
+                    x0+=1
+                    d += 2*a
+
                 y0+=1
                 d += 2*b
 
-            x0+=1
-            d += 2*a
     else:
+        if(abs(a) <= abs(b)): # IF dy <= dx, slope <= 1
+            #Octant IV, VIII
+            d = 2*a-b
 
-        #Octant II
-        d = a+2*b
+            while x0 <= x1:
 
-        while y0 <= y1:
+                #PLOT
+                plot(screen, color, x0, y0)
+                if d < 0:
+                    y0-=1
+                    d -= 2*b
 
-            #PLOT
-            plot(screen, color, x0, y0)
-            if d < 0:
                 x0+=1
                 d += 2*a
+        else:
+            #Octant III, VII
+            d = a-2*b
 
-            y0+=1
-            d += 2*b
+            while y0 >= y1:
+
+                #PLOT
+                plot(screen, color, x0, y0)
+                if d > 0:
+                    x0+=1
+                    d += 2*a
+
+                y0-=1
+                d -= 2*b
--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 from display import *
 from draw import *
+from subprocess import Popen, PIPE
 
 screen = new_screen()
 
-# OCTANT 1 TEST
-#color = [ 0, 255, 0 ]
-# draw_line( 0, 0, 1000, 250, screen, color)
-# draw_line( 0, 0, 1000, 500, screen, color)
-# draw_line( 0, 0, 1000, 750, screen, color)
-# draw_line( 0, 0, 1000, 1000, screen, color)
+# Horizontal + Vertical Line Tests
+color = [ 255, 255, 255 ]
+draw_line( 250, -500, 250, 500, screen, color)
+color = [ 255, 255, 0 ]
+draw_line( -500, 250, 500, 250, screen, color)
 
-# OCTANT 2 TEST
+
+
+
+# OCTANT 1,5 TEST
+color = [ 0, 255, 0 ]
+draw_line( 0, 0, 1000, 250, screen, color)
+draw_line( 0, 0, 1000, 500, screen, color)
+
+# OCTANT 2, 6 TEST
 color = [ 255, 0, 0 ]
 draw_line( 0, 0, 250, 1000, screen, color)
 draw_line( 0, 0, 500, 1000, screen, color)
-draw_line( 0, 0, 750, 1000, screen, color)
-draw_line( 0, 0, 10, 1000, screen, color)
+
+# OCTANT 3, 7 TEST
+color = [0, 122, 255 ]
+draw_line( 0, 500, 250,0, screen, color)
+draw_line( 250, 500, 500,100, screen, color)
+
+# OCTANT 4, 8 TEST
+color = [ 255, 122, 0 ]
+draw_line( 250, 500, 500, 0, screen, color)
+draw_line( 0, 400, 500, 0, screen, color)
 
 
 
 display(screen)
 save_extension(screen, 'img.png')
+# p = Popen( ['open', "img.png"], stdin=PIPE, stdout = PIPE )
+# p.communicate()
